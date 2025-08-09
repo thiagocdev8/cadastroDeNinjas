301b746b969cc3b30ac586390202bd798c8567c8
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-08-09 17:57:52 -0300

    adding amend

--- src/main/java/dev/java10x/cadastroDeNinjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/NinjaModel.java
@@ -10,10 +10,10 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel() {
     }
