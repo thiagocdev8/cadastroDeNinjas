8e148758d01c33b47d27653fad305818d3bc9b36
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-08-09 17:46:21 -0300

    adding user entity (ninjas)

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.java10x.cadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//entity ele transforma uma classe em uma entidade do banco de DADOS
+//JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String email, String nome, int idade) {
+        this.email = email;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
