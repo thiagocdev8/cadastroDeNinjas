feaa2f0206956670a8a1bcde5a74cb9bbf62d648
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-08-09 16:59:11 -0300

    testing initial route

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.cadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
